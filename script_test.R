36aa1993b0d3a8f8b304eac0c5ca7c91f0ad2e09
Subhendu Mahapatro <the.subhendum@gmail.com>
2020-10-18 12:12:32 +0530

    part 2 complete

--- script_test.R
+++ script_test.R
@@ -159,3 +159,51 @@ cutoff2[which.max(accuracy2)]
 y_hat <- ifelse(test$Petal.Length > 4.7 | test$Petal.Width > 1.5, "virginica", "versicolor") %>%
   factor(levels = levels(test$Species))
 mean(y_hat == test$Species)
+
+#cc03
+
+options(digits = 3)
+(.85*.02)/(.85*.02+.1*.98)
+
+set.seed(1, sample.kind = "Rounding")
+disease <- sample(c(0,1), size=1e6, replace=TRUE, prob=c(0.98,0.02))
+test <- rep(NA, 1e6)
+test[disease==0] <- sample(c(0,1), size=sum(disease==0), replace=TRUE, prob=c(0.90,0.10))
+test[disease==1] <- sample(c(0,1), size=sum(disease==1), replace=TRUE, prob=c(0.15, 0.85))
+
+.85*.02+.1*.98
+mean(test)
+mean(disease[test==0])
+(.85*.02)/(.85*.02+.1*.98)
+mean(disease[test==1])
+mean(disease[test==1])/.02
+(.85*.02)/(.85*.02+.1*.98)/.02
+
+library(dslabs)
+data("heights")
+heights %>% 
+  mutate(height = round(height)) %>%
+  group_by(height) %>%
+  summarize(p = mean(sex == "Male")) %>%
+  qplot(height, p, data =.)
+ggsave("plots/height_condition_male_qplot.jpg")
+
+ps <- seq(0, 1, 0.1)
+heights %>% 
+  mutate(g = cut(height, quantile(height, ps), include.lowest = TRUE)) %>%
+  group_by(g) %>%
+  summarize(p = mean(sex == "Male"), height = mean(height)) %>%
+  qplot(height, p, data =.)
+ggsave("plots/height_condition_male_qplot_quantile.jpg")
+
+Sigma <- 9*matrix(c(1,0.5,0.5,1), 2, 2)
+dat <- MASS::mvrnorm(n = 10000, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+ps <- seq(0, 1, 0.1)
+ps <- seq(0, 1, 0.1)
+dat %>% 
+  mutate(g = cut(x, quantile(x, ps), include.lowest = TRUE)) %>%
+  group_by(g) %>%
+  summarize(y = mean(y), x = mean(x)) %>%
+  qplot(x, y, data =.)
+ggsave("plots/height_bivariate_extraction.jpg")

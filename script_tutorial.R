7ea95a7a6969c5cfa9583cde586e16752aaf79b4
Subhendu Mahapatro <the.subhendum@gmail.com>
2020-10-31 11:26:26 +0530

    3.1 done

--- script_tutorial.R
+++ script_tutorial.R
@@ -165,3 +165,71 @@ ggsave("plots/heights_proportion_female_general_regression_line.jpg")
 y_hat_logit <- ifelse(p_hat_logit > 0.5, "Female", "Male") %>% factor
 confusionMatrix(y_hat_logit, test_set$sex)$overall[["Accuracy"]]
 confusionMatrix(y_hat_logit, test_set$sex)
+
+# Case study 2 or 7
+
+mnist <- read_mnist()
+is <- mnist_27$index_train[c(which.min(mnist_27$train$x_1), which.max(mnist_27$train$x_1))]
+titles <- c("smallest","largest")
+tmp <- lapply(1:2, function(i){
+  expand.grid(Row=1:28, Column=1:28) %>%
+    mutate(label=titles[i],
+           value = mnist$train$images[is[i],])
+})
+tmp <- Reduce(rbind, tmp)
+tmp %>% ggplot(aes(Row, Column, fill=value)) +
+  geom_raster() +
+  scale_y_reverse() +
+  scale_fill_gradient(low="white", high="black") +
+  facet_grid(.~label) +
+  geom_vline(xintercept = 14.5) +
+  geom_hline(yintercept = 14.5)
+ggsave("plots/7_vs_2.jpg")
+data("mnist_27")
+mnist_27$train %>% ggplot(aes(x_1, x_2, color = y)) + geom_point()
+ggsave("plots/7_vs_2_scatter.jpg")
+is <- mnist_27$index_train[c(which.min(mnist_27$train$x_2), which.max(mnist_27$train$x_2))]
+titles <- c("smallest","largest")
+tmp <- lapply(1:2, function(i){
+  expand.grid(Row=1:28, Column=1:28) %>%
+    mutate(label=titles[i],
+           value = mnist$train$images[is[i],])
+})
+tmp <- Reduce(rbind, tmp)
+tmp %>% ggplot(aes(Row, Column, fill=value)) +
+  geom_raster() +
+  scale_y_reverse() +
+  scale_fill_gradient(low="white", high="black") +
+  facet_grid(.~label) +
+  geom_vline(xintercept = 14.5) +
+  geom_hline(yintercept = 14.5)
+ggsave("plots/7_vs_7.jpg")
+fit_glm <- glm(y ~ x_1 + x_2, data=mnist_27$train, family = "binomial")
+p_hat_glm <- predict(fit_glm, mnist_27$test)
+y_hat_glm <- factor(ifelse(p_hat_glm > 0.5, 7, 2))
+confusionMatrix(data = y_hat_glm, reference = mnist_27$test$y)$overall["Accuracy"]
+confusionMatrix(data = y_hat_glm, reference = mnist_27$test$y)
+
+mnist_27$true_p %>% ggplot(aes(x_1, x_2, fill=p)) +
+  geom_raster()
+ggsave("plots/7_vs_2_true_p.jpg")
+mnist_27$true_p %>% ggplot(aes(x_1, x_2, z=p, fill=p)) +
+  geom_raster() +
+  scale_fill_gradientn(colors=c("#F8766D","white","#00BFC4")) +
+  stat_contour(breaks=c(0.5), color="black")
+ggsave("plots/7_vs_2_true_p_boundary.jpg")
+p_hat <- predict(fit_glm, newdata = mnist_27$true_p)
+mnist_27$true_p %>%
+  mutate(p_hat = p_hat) %>%
+  ggplot(aes(x_1, x_2,  z=p_hat, fill=p_hat)) +
+  geom_raster() +
+  scale_fill_gradientn(colors=c("#F8766D","white","#00BFC4")) +
+  stat_contour(breaks=c(0.5),color="black")
+ggsave("plots/7_vs_2_p_boundary.jpg")
+p_hat <- predict(fit_glm, newdata = mnist_27$true_p)
+mnist_27$true_p %>%
+  mutate(p_hat = p_hat) %>%
+  ggplot() +
+  stat_contour(aes(x_1, x_2, z=p_hat), breaks=c(0.5), color="black") +
+  geom_point(mapping = aes(x_1, x_2, color=y), data = mnist_27$test)
+ggsave("plots/7_vs_2_p.jpg")

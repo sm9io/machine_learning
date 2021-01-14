a25ea08464c5194becf9ba8ae0ac6d507373217c
Subhendu Mahapatro <the.subhendum@gmail.com>
2021-01-14 20:45:28 +0530

    6.1 done

--- script_tutorial.R
+++ script_tutorial.R
@@ -915,3 +915,40 @@ for(i in 1:12){
         xaxt="n", yaxt="n")
 }
 ggsave("plots/mnist_rf_predict_actual.jpg")
+
+library(randomForest)
+x <- mnist$train$images[index,]
+y <- factor(mnist$train$labels[index])
+rf <- randomForest(x, y,  ntree = 50)
+imp <- importance(rf)
+imp
+
+image(matrix(imp, 28, 28))
+ggsave("plots/mnist_rf_importance.jpg")
+
+p_max <- predict(fit_knn, x_test[,col_index])
+p_max <- apply(p_max, 1, max)
+ind  <- which(y_hat_knn != y_test)
+ind <- ind[order(p_max[ind], decreasing = TRUE)]
+rafalib::mypar(3,4)
+for(i in ind[1:12]){
+  image(matrix(x_test[i,], 28, 28)[, 28:1],
+        main = paste0("Pr(",y_hat_knn[i],")=",round(p_max[i], 2),
+                      " but is a ",y_test[i]),
+        xaxt="n", yaxt="n")
+}
+ggsave("plots/mnist_knn_predict_actual_fail.jpg")
+
+p_max <- predict(fit_rf, x_test[,col_index])$census  
+p_max <- p_max / rowSums(p_max)
+p_max <- apply(p_max, 1, max)
+ind  <- which(y_hat_rf != y_test)
+ind <- ind[order(p_max[ind], decreasing = TRUE)]
+rafalib::mypar(3,4)
+for(i in ind[1:12]){
+  image(matrix(x_test[i,], 28, 28)[, 28:1], 
+        main = paste0("Pr(",y_hat_rf[i],")=",round(p_max[i], 2),
+                      " but is a ",y_test[i]),
+        xaxt="n", yaxt="n")
+}
+ggsave("plots/mnist_rf_predict_actual_fail.jpg")

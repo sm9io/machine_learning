8e55996f3e6a495f766343dbc2e2bf276b7fd0f5
Subhendu Mahapatro <the.subhendum@gmail.com>
2021-01-14 16:26:08 +0530

    6.1 wip

--- script_tutorial.R
+++ script_tutorial.R
@@ -819,3 +819,99 @@ confusionMatrix(data = predict(train_loess, mnist_27$test),
 p1 <- plot_cond_prob(predict(train_loess, mnist_27$true_p, type = "prob")[,2])
 p1
 ggsave("plots/27_loess_x1_x2_cond_p.jpg")
+
+# section 6
+
+#section 6.1
+
+library(dslabs)
+mnist <- read_mnist()
+
+names(mnist)
+dim(mnist$train$images)
+
+class(mnist$train$labels)
+table(mnist$train$labels)
+
+# sample 10k rows from training set, 1k rows from test set
+set.seed(123, sample.kind = "Rounding")
+index <- sample(nrow(mnist$train$images), 10000)
+x <- mnist$train$images[index,]
+y <- factor(mnist$train$labels[index])
+
+index <- sample(nrow(mnist$test$images), 1000)
+
+x_test <- mnist$test$images[index,]
+y_test <- factor(mnist$test$labels[index])
+
+library(matrixStats)
+sds <- colSds(x)
+qplot(sds, bins = 256, color = I("black"))
+ggsave("plots/mnist_column_sd_hist.jpg")
+
+library(caret)
+nzv <- nearZeroVar(x)
+image(matrix(1:784 %in% nzv, 28, 28))
+ggsave("plots/mnist_column_nzv.jpg")
+
+col_index <- setdiff(1:ncol(x), nzv)
+length(col_index)
+
+colnames(x) <- 1:ncol(mnist$train$images)
+colnames(x_test) <- colnames(x)
+
+control <- trainControl(method = "cv", number = 10, p = .9)
+train_knn <- train(x[,col_index], y,
+                   method = "knn", 
+                   tuneGrid = data.frame(k = c(1,3,5,7)),
+                   trControl = control)
+ggplot(train_knn)
+ggsave("plots/mnist_knn_neighbours_accuracy.jpg")
+
+n <- 1000
+b <- 2
+index <- sample(nrow(x), n)
+control <- trainControl(method = "cv", number = b, p = .9)
+train_knn <- train(x[index ,col_index], y[index],
+                   method = "knn",
+                   tuneGrid = data.frame(k = c(3,5,7)),
+                   trControl = control)
+fit_knn <- knn3(x[ ,col_index], y,  k = 3)
+
+y_hat_knn <- predict(fit_knn,
+                     x_test[, col_index],
+                     type="class")
+cm <- confusionMatrix(y_hat_knn, factor(y_test))
+cm$overall["Accuracy"]
+
+cm$byClass[,1:2]
+
+library(Rborist)
+control <- trainControl(method="cv", number = 5, p = 0.8)
+grid <- expand.grid(minNode = c(1,5) , predFixed = c(10, 15, 25, 35, 50))
+train_rf <-  train(x[, col_index], y,
+                   method = "Rborist",
+                   nTree = 50,
+                   trControl = control,
+                   tuneGrid = grid,
+                   nSamp = 5000)
+ggplot(train_rf)
+ggsave("plots/mnist_rf_predictor_accuracy.jpg")
+train_rf$bestTune
+
+fit_rf <- Rborist(x[, col_index], y,
+                  nTree = 1000,
+                  minNode = train_rf$bestTune$minNode,
+                  predFixed = train_rf$bestTune$predFixed)
+
+y_hat_rf <- factor(levels(y)[predict(fit_rf, x_test[ ,col_index])$yPred])
+cm <- confusionMatrix(y_hat_rf, y_test)
+cm$overall["Accuracy"]
+
+rafalib::mypar(3,4)
+for(i in 1:12){
+  image(matrix(x_test[i,], 28, 28)[, 28:1], 
+        main = paste("Our prediction:", y_hat_rf[i]),
+        xaxt="n", yaxt="n")
+}
+ggsave("plots/mnist_rf_predict_actual.jpg")
